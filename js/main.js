6ff5a74eedba1a3646da4d4f618034593a76a5a3
Y.Panteleev <y-panteleew@yandex.ru>
2021-11-09 13:13:00 +0400

    Добавлены условия и проверки на форму опроса

--- js/main.js
+++ js/main.js
@@ -15,38 +15,52 @@ function isFloat(number) {
 let checkNum = true;
 let countMovies;
 while(checkNum){
-
     countMovies = Number(prompt("Сколько фильмов вы посмотрели?", ""));
-
     if(Number.isInteger(countMovies) && countMovies > 0){
         numberOfFilm = countMovies;
         personalMovieDB.count = numberOfFilm;
         checkNum = false;
     }else{
-        alert("Введено неправильное значение). Укажите число фильмов цифрой");
+        alert("Введено неправильное значение. Укажите число фильмов цифрой");
     }
 }
 
-
 let movie;
 let rating;
-for(let i = 1; i <= numberOfFilm; i++){
-
-    movie =  prompt("Фильм который вы уже посмотрели", "");
+for(let i = 1; i <= 2; i++){
+    let movieLengthCheck = true;
+    while(movieLengthCheck){
+        movie =  prompt("Фильм который вы уже посмотрели", "");
+        if (movie.length < 50 && movie.length > 1) {
+            movieLengthCheck = false;
+        } else {
+            alert('Вы не можете указать название длинее 50 и менее 2-х символов');
+        }
+    }
 
     let ratingCheck = true;
 
     while(ratingCheck) {
         rating = Number(prompt("Оцените фильм по шкале от 1.0 до 10", ''));
-        if(rating <= 10 || rating >= 1 || isFloat(rating) || Number.isInteger(rating)) {
+        
+        if(rating <= 10 && rating >= 1 && (isFloat(rating) || Number.isInteger(rating))) {
             personalMovieDB.movies[movie] = rating; 
-            console.log(typeof(rating));
             ratingCheck = false;
         }else {
             alert('Оценка введена некорректно, формат оценки от 1.0 до 10, например 5.5');
         }
     }
 
+}
+
+if(personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман!');
+}else {
+    alert('Произошла ошибк51518а' + personalMovieDB.count);
 }
 
 console.log(personalMovieDB);
